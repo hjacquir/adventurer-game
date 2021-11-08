7fd5bce6698a6934e68948c78bf968a3d077a21b
hjacquir <jacquirhatim@gmail.com>
2021-11-08 22:33:40 +0100

    domain : add latitude line

---
+++ src/Domain/LatitudeLine.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain;
+
+class LatitudeLine
+{
+    private string $line;
+    private int $latitude;
+
+    /**
+     * @param string $line
+     * @param int $latitude
+     */
+    public function __construct(string $line, int $latitude)
+    {
+        $this->line = $line;
+        $this->latitude = $latitude;
+    }
+
+    public function getLine(): string
+    {
+        return $this->line;
+    }
+
+    public function getLatitude(): int
+    {
+        return $this->latitude;
+    }
+}
